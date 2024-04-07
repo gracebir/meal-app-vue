346269101fb353c6a7effbd109aaac909cd291c4
gracebir <grace.devolop@gmail.com>
2024-04-07 17:38:10 +0200

    get started

---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+/** @format */
+
+import { createStore } from "vuex";
+import state from "./state";
+import actions from "./actions";
+import getters from "./getters";
+import mutations from "./mutations";
+
+const store = createStore({
+    state,
+    actions,
+    mutations,
+    getters,
+});
+
+export default store;
---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h1 class="text-xl font-bold">Home page</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
