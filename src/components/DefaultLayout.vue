ee2c43fe3f86050ca4ea87cb48401c92904e9035
gracebir <grace.devolop@gmail.com>
2024-04-10 18:07:11 +0200

    default layout compoent

---
+++ src/components/DefaultLayout.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="min-h-screen bg-gray-300 w-full font-poppins">
+        <nav-bar />
+        <main class="max-w-7xl mx-auto px-8">
+            <router-view />
+        </main>
+    </div>
+</template>
+
+<script setup>
+import NavBar from './NavBar.vue';
+
+</script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,24 @@
+<template>
+    <header class="w-full bg-white h-16 shadow">
+        <div class="max-w-7xl mx-auto py-4 text-lg flex justify-between px-8">
+            <router-link :to="{ name: 'home' }">
+                <span class="font-bold">Meal App</span>
+            </router-link>
+
+            <div class="flex gap-8 text-base">
+                <router-link class="hover:underline duration-300 font-medium text-gray-600 hover:text-gray-800"
+                    :to="{ name: 'byName' }">
+                    Search Meals
+                </router-link>
+                <router-link :to="{ name: 'byLetter' }"
+                    class="hover:underline duration-300 font-medium text-gray-600 hover:text-gray-800">
+                    Meals By Letter
+                </router-link>
+                <router-link :to="{ name: 'MealByName' }"
+                    class="hover:underline duration-300 font-medium text-gray-600 hover:text-gray-800">
+                    Meals By Ingredient
+                </router-link>
+            </div>
+        </div>
+    </header>
+</template>
