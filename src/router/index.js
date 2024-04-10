f2636141ebba827d9177be978c08b9b3d1e636d1
gracebir <grace.devolop@gmail.com>
2024-04-10 18:06:51 +0200

    route config

--- src/router/index.js
+++ src/router/index.js
@@ -3,17 +3,42 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import MealList from "../views/MealList.vue";
+import SearchByName from "../views/SearchByName.vue";
+import SearchByLetter from "../views/SearchByLetter.vue";
+import SearchByIngredient from "../views/SearchByIngredient.vue";
+import DefaultLayout from "../components/DefaultLayout.vue";
 
 const routes = [
     {
         path: "/",
-        name: "home",
-        component: Home,
-    },
-    {
-        path: "/letter/:letter",
-        name: "byLetter",
-        component: MealList,
+        component: DefaultLayout,
+        children: [
+            {
+                path: "/",
+                name: "home",
+                component: Home,
+            },
+            {
+                path: "/letter/:letter?",
+                name: "byLetter",
+                component: MealList,
+            },
+            {
+                path: "/by-name/:name?",
+                name: "byName",
+                component: SearchByName,
+            },
+            {
+                path: "/by-ingredient/:ingredient?",
+                name: "byIngredient",
+                component: SearchByIngredient,
+            },
+            {
+                path: "/by-meals/:name?",
+                name: "MealByName",
+                component: SearchByLetter,
+            },
+        ],
     },
 ];
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@
 export default {
     content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                poppins: ["Poppins", "sans-serif"],
+            },
+        },
     },
     plugins: [require("@tailwindcss/forms")],
 };
