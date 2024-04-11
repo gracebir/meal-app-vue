9c79c596afdb138895909811064d915cf1428d4f
gracebir <grace.devolop@gmail.com>
2024-04-11 08:17:15 +0200

    search meals spli into component

--- src/views/SearchByName.vue
+++ src/views/SearchByName.vue
@@ -1,25 +1,29 @@
 <template>
     <div>
+
         <div class="py-8 w-full">
             <input v-model="keyword" @change="searchMeal" placeholder="Search meals" type="text"
                 class="rounded border-gray-300 w-full">
+
             <div class="grid grid-cols-1 md:grid-cols-3 gap-6 my-8">
+
                 <!-- food card -->
                 <div class="bg-white shadow-md rounded-lg w-full" v-for="meal of meals" :key="meal.idMeal">
-                    <div class="h-48 w-full">
-                        <img class="rounded-t-lg w-full h-full object-cover" :src="meal.strMealThumb"
-                            :alt="meal.strTags">
-                    </div>
+                    <router-link :to="{ name: 'MealDetail', params: { id: meal.idMeal } }">
+                        <div class="h-48 w-full">
+                            <img class="rounded-t-lg w-full h-full object-cover" :src="meal.strMealThumb"
+                                :alt="meal.strTags">
+                        </div>
+                    </router-link>
                     <div class="p-4 flex flex-col gap-5">
                         <h3 class="font-semibold text-xl">{{ meal.strMeal }}</h3>
                         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic, veniam dolorum quas facere
                             natus velit, eum maiores vel officiis nihil alias obcaecati eveniet atque omnis. </p>
                         <div class="flex justify-between items-center">
-                            <a class="border-red-300 border-2 px-4 py-1 text-red-300 hover:text-white font-medium duration-300 hover:bg-red-300  rounded-lg"
-                                :href="meal.strYoutube">Youtube</a>
+                            <Youtube :href="meal.strYoutube" text="Youtube" />
                             <router-link
                                 class="border-blue-300 border-2 px-4 py-1 text-blue-300 hover:text-white font-medium duration-300 hover:bg-blue-300  rounded-lg"
-                                to="/">
+                                :to="{ name: 'MealDetail', params: { id: meal.idMeal } }">
                                 View
                             </router-link>
                         </div>
@@ -29,10 +33,12 @@
         </div>
     </div>
 </template>
+
 <script setup>
 import { computed, onMounted, ref } from 'vue'
 import store from '../store';
 import { useRoute } from 'vue-router';
+import Youtube from '../components/Youtube.vue';
 
 const meals = computed(() => store.state.searchedMeals)
 const route = useRoute()
